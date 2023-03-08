509ad6273d6db2bb3b8eb54fe2454ab6a50ce94f
Wesley Neves <wesley.neves.matos@gmail.com>
2023-03-08 20:14:11 -0300

    Criando a classe Posicao

--- xadrex-console/Program.cs
+++ xadrex-console/Program.cs
@@ -1,10 +1,14 @@
 ﻿
+using Tabuleiro;
+
 namespace xadrez_console
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Posicao p = new Posicao(3, 4);
+            Console.WriteLine( "Posição: " + p);
         }
     }
 }
